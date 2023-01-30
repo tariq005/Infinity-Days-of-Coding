3934dcd11faaac3f19798ed404546f7ed88a6ec5
Md Tariq Hasan Siddique <114738441+tariq005@users.noreply.github.com>
2023-01-30 15:28:36 +0600

    Add files via upload
    from CF

---
+++ number_factorization.py
@@ -0,0 +1,36 @@
+import sys
+input= sys.stdin.readline
+for _ in range(int(input())):
+    n= int(input())
+    a= {}
+    i= 2
+
+    while i*i <= n:
+        while n%i == 0:
+            if i not in a:
+                a[i]= 1
+            else:
+                a[i] += 1
+            n //= i
+        i += 1
+    if n>1:
+        if n in a:
+            a[n] += 1
+        else:
+            a[n]= 1
+
+    factor= 1
+    prime= 0
+    p= set(a.values())
+    while len(set(a.values())) >=1:
+        if len(set(a.values())) == 1 and 0 in set(a.values()):
+            break
+        else:
+            for x in a:
+                if a[x]>0:
+                    factor *= x
+                    a[x] -= 1
+            prime += factor
+            factor= 1
+
+    print(prime)
