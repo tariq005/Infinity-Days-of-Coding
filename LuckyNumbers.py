04cb2a1aff515adf68ab5af84fe1192eb27f4321
Md Tariq Hasan Siddique <114738441+tariq005@users.noreply.github.com>
2023-03-30 14:56:02 +0600

    Update LuckyNumbers.py

--- LuckyNumbers.py
+++ LuckyNumbers.py
@@ -1,7 +1,7 @@
 for _ in range(int(input())):
-    l, r= map(str, input().split())
-    a, b, diff, x= int(l), int(r), 0, int(l)
-    for i in range(a, b+1):
+    l, r= map(int, input().split())
+    diff, x= 0, l
+    for i in range(l, r+1):
         s= str(i)
         p= int(max(s))-int(min(s))
         if p>diff:
