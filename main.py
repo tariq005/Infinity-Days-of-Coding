c5f6b6323096d2e5e3cd1889b437f7660e6c24c4
Md Tariq Hasan Siddique <114738441+tariq005@users.noreply.github.com>
2023-05-01 15:45:37 +0600

    Add files via upload
    Mahmoud & Ehab and the message from CF

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-n= int(input())
-a= []
+n, k, m= map(int, input().split())
+index= {x: i for i, x in enumerate(input().split())}
+a= list(map(int, input().split()))
 
-for x in range(max(1, n-99), n+1):
-    print(x)
-    if x+ sum(map(int, str(x))) == n:
-        a.append(x)
+for i in range(k):
+    b= [int(x)-1 for x in input().split()[1:]]
+    z= min(a[x] for x in b)
+    for x in b:
+        a[x]= z
 
-print(len(a))
-print(*a)
+print(sum(a[index[x]] for x in input().split()))
