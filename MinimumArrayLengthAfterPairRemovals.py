3ae2512b0258338de8e5ae6fc7a01006655c7af0
Md Tariq Hasan Siddique <114738441+tariq005@users.noreply.github.com>
2023-10-05 00:07:06 +0600

    Create MinimumArrayLengthAfterPairRemovals.py
    from LC

---
+++ MinimumArrayLengthAfterPairRemovals.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minLengthAfterRemovals(self, nums: List[int]) -> int:
+        n = len(nums)
+        i = nums[n//2]
+        cnt = bisect_right(nums, i) - bisect_left(nums, i)
+        return max(n%2, 2*cnt-n)
